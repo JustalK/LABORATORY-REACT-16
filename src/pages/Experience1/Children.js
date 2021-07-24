cb4c249beb435c0f9505f361fc20021529a773aa
Latsuj <justal.kevin@gmail.com>
2021-07-24 23:51:41 +0800

    [TEST] Profiler from Laboratory

---
+++ src/pages/Experience1/Children.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Children = ({ val }) => {
+  return <div>{val}</div>
+}
+
+export default Children
--- src/pages/Experience1/index.js
+++ src/pages/Experience1/index.js
@@ -3,21 +3,48 @@
  * @module Home
  */
 
-import React from 'react'
-import Seo from '@components/Seo'
+import React, { Profiler, useState } from 'react'
+import Children from '@pages/Experience1/Children'
 
 /**
- * @function Home
+ * @function Experience
  * Create the component Home
  * @return {Object} Return the dom of the Home page
  */
-const Home = () => {
+const Experience = () => {
+  const [val, setVal] = useState(0)
+
+  const callback = (
+    id,
+    phase,
+    actualDuration,
+    baseDuration,
+    startTime,
+    commitTime,
+    interactions
+  ) => {
+    console.log(
+      id,
+      phase,
+      actualDuration,
+      baseDuration,
+      startTime,
+      commitTime,
+      interactions
+    )
+  }
+
+  console.log('Re-render')
   return (
-    <>
-      <Seo title="Home" description="Description of Home" />
-      Test
-    </>
+    <Profiler id="Incrementation" onRender={callback}>
+      <div>
+        <button onClick={() => setVal((c) => c + 1)}>
+          Increment and look console
+        </button>
+        <Children val={val} />
+      </div>
+    </Profiler>
   )
 }
 
-export default Home
+export default Experience
