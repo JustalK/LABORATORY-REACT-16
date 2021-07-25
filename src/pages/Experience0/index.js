d50b7ac64fd0d6dce7ec33b0a9c8a308380659be
Latsuj <justal.kevin@gmail.com>
2021-07-25 11:18:54 +0800

    [FEATURE] Adding a template experience

---
+++ src/pages/Experience0/index.js
@@ -0,0 +1,21 @@
+/**
+ * Profile the app for knowing when everything re-render
+ * @module Experiences/Experience0
+ */
+
+import React from 'react'
+
+/**
+ * @function Experience
+ * Profile the app for knowing when everything re-render
+ * @return {Object} Return the dom of the Experience
+ */
+const Experience = () => {
+  return (
+    <div>
+      This is a test - This is a test - This is a test - This is a test -
+    </div>
+  )
+}
+
+export default Experience
